45eefa7dc3b8182ad5af0e2b5f51362af05f561f
Madan Gowda S P <madanspgowda@gmail.com>
2026-02-28 17:45:42 +0500

    complete table

--- src/main/java/com/example/orderByname/UserController.java
+++ src/main/java/com/example/orderByname/UserController.java
@@ -2,9 +2,7 @@ package com.example.orderByname;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -25,5 +23,34 @@ public class UserController {
         redirectAttributes.addFlashAttribute("success","Succefully Registerd");
         userser.saveuserdata(userentity);
         return "redirect:/";
+    }
+    @GetMapping("/datanamepage")
+    public String datanamepageshow(Model model,@RequestParam(defaultValue = "asc") String direction){
+
+        model.addAttribute("datapagelist",userser.datapagename(direction));
+        model.addAttribute("direction",direction);
+        return "datanamepage";
+    }
+    @GetMapping("/delete/{id}")
+    public String deletedata(@PathVariable Long id){
+        userser.deleterecoeds(id);
+        return "redirect:/datanamepage";
+    }
+    @GetMapping("/edit/{id}")
+    public String editbyid(@PathVariable Long id,Model model){
+        model.addAttribute("formdata",  userser.editbyid(id));
+        return "formpage";
+    }
+    @PostMapping("/edit")
+    public String updateUser(@ModelAttribute UserEntity userentity,
+                             RedirectAttributes redirectAttributes){
+        userser.saveuserdata(userentity);   // same save() method
+        redirectAttributes.addFlashAttribute("success","Successfully Updated");
+        return "redirect:/datanamepage";
+    }
+    @GetMapping("/deleteall")
+    public String deleteallrecoeds(){
+        userser.deleteallrecords();
+        return "redirect:/datanamepage";
     }
 }
--- src/main/java/com/example/orderByname/UserServices.java
+++ src/main/java/com/example/orderByname/UserServices.java
@@ -1,6 +1,9 @@
 package com.example.orderByname;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserServices {
@@ -11,5 +14,24 @@ public class UserServices {
     }
     public UserEntity saveuserdata(UserEntity userEntity){
         return userrepo.save(userEntity);
+    }
+    public List<UserEntity> datapagename(String direction){
+        Sort sort;
+        if(direction.equals("asc")){
+           sort= Sort.by("name").ascending();
+        }
+        else{
+           sort= Sort.by("name").descending();
+        }
+        return userrepo.findAll(sort);
+    }
+    public void deleterecoeds(Long id){
+         userrepo.deleteById(id);
+    }
+    public UserEntity editbyid(Long id){
+        return userrepo.findById(id).orElseThrow();
+    }
+    public void deleteallrecords(){
+        userrepo.deleteAll();
     }
 }
